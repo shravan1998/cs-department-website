41fb4757e4fcf0623588b4917378997cb86fd670
shravan1998 <shrkum51@gmail.com>
2019-08-24 14:09:24 +0530

    Achievements page frontent complete

--- cs-department-website/src/app/app-routing.module.ts
+++ cs-department-website/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component';
+import { AchievementsComponent } from './achievements/achievements.component';
 
 
 const routes: Routes = [{
   path:'',component:HomeComponent
+},{
+  path:'achievements',component:AchievementsComponent
 }];
 
 @NgModule({
