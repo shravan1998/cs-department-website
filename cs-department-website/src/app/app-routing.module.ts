e95f9fd29460d1cb63e74ea0ff70053bf4b46ca8
shravan1998 <shrkum51@gmail.com>
2019-08-26 09:58:48 +0530

    Events component added

--- cs-department-website/src/app/app-routing.module.ts
+++ cs-department-website/src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import { AchievementsComponent } from './achievements/achievements.component';
+import {EventsComponent} from './events/events.component';
 
 
 const routes: Routes = [{
   path:'',component:HomeComponent
 },{
   path:'achievements',component:AchievementsComponent
+},{
+  path:'events',component:EventsComponent
 }];
 
 @NgModule({
