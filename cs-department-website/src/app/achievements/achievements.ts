a0f2564382aac4af77fc562dabe5d62c2dd2b970
shravan1998 <shrkum51@gmail.com>
2019-09-19 10:51:58 +0530

    File achievements.ts removed

--- cs-department-website/src/app/achievements/achievements.ts
+++
@@ -1,8 +0,0 @@
-export interface IAchievements{
-    name: string,
-    usn: string,
-    Year_of_admission: number,
-    competition: string,
-    Win: string,
-    date_of_participation: string
-}
